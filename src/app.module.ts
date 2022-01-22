c1a7c5a19c1e80a1ec9c8acb6fdbf5adb6b27d44
almu <muzyka_aleksandr.n@mail.ru>
2022-01-22 23:07:15 +0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+      logging: true
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
